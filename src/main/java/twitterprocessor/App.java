a0d3bf2e59998df839b6b012b9b9d2d2e312a8ff
Sam Pullara <spullara@yahoo.com>
2013-03-10 15:12:05 -0700

    first commit

---
+++ src/main/java/twitterprocessor/App.java
@@ -0,0 +1,144 @@
+package twitterprocessor;
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.MappingJsonFactory;
+import com.sampullara.cli.Args;
+import com.sampullara.cli.Argument;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+import java.util.stream.FlatMapper;
+import java.util.stream.Stream;
+import java.util.stream.Streams;
+import java.util.zip.GZIPInputStream;
+
+import static java.util.stream.ConcurrentCollectors.groupingReduce;
+
+/**
+ * Read and process a feed of compressed tweets
+ */
+public class App {
+
+  @Argument(alias = "f", description = "File or s3 url", required = true)
+  private static String file;
+
+  @Argument(alias = "a", description = "AWS properties file specifying accessKey and secretKey")
+  private static File auth;
+
+  @Argument(alias = "p", description = "Number of processor threads")
+  private static Integer processors = 2;
+
+  private static Logger squelchLog = Logger.getLogger("Squelched");
+
+  public static void main(String[] args) throws IOException, URISyntaxException {
+    try {
+      Args.parse(App.class, args);
+    } catch (IllegalArgumentException e) {
+      System.err.println(e.getMessage());
+      Args.usage(App.class);
+      System.exit(1);
+    }
+
+    MappingJsonFactory jf = new MappingJsonFactory();
+
+    FlatMapper<String, JsonNode> lineToJson = squelch((line, consumer) -> consumer.accept(jf.createParser(line).readValueAsTree()));
+
+    Pattern noProtocol = Pattern.compile("^[A-Za-z0-9-]+[.]");
+
+    FlatMapper<String, URL> toURL = squelch((link, consumer) -> {
+      if (noProtocol.matcher(link).find()) {
+        consumer.accept(new URL("http://" + link));
+      } else {
+        consumer.accept(new URL(link));
+      }
+    });
+
+    FlatMapper<JsonNode, String> toLinks = (tweet, consumer) -> {
+      JsonNode links = tweet.get("l");
+      if (links != null) {
+        links.forEach(link -> consumer.accept(link.textValue()));
+      }
+    };
+
+    long start = System.currentTimeMillis();
+
+    InputStream in;
+    if (file.startsWith("s3://")) {
+      if (auth == null) {
+        throw new IllegalArgumentException("You must specify an auth properties file for AWS");
+      } else {
+        Properties aws = new Properties();
+        aws.load(new FileInputStream(auth));
+        URI url = new URI(file);
+        GetObjectRequest get = new GetObjectRequest(url.getHost(), url.getPath().substring(1));
+        AmazonS3Client client = new AmazonS3Client(new BasicAWSCredentials(aws.getProperty("accessKey"), aws.getProperty("secretKey")));
+        S3Object object = client.getObject(get);
+        in = object.getObjectContent();
+      }
+    } else {
+      in = new FileInputStream(file);
+    }
+
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new GZIPInputStream(in), "UTF-8"));
+    Stream<String> lines = bufferedReader.lines();
+    AtomicInteger tweets = new AtomicInteger();
+    AtomicInteger links = new AtomicInteger();
+    ConcurrentMap<String, Integer> map = Streams.parallelStream(new PullSpliterator<String>(lines, processors))
+            .peek(a -> {
+              int i = tweets.incrementAndGet();
+              if (i % 100000 == 0) {
+                System.out.println("Processed " + i + " " + i / (System.currentTimeMillis() - start));
+              }
+            })
+            .flatMap(lineToJson)
+            .flatMap(toLinks)
+            .peek(a -> links.incrementAndGet())
+            .flatMap(toURL)
+            .collectUnordered(groupingReduce(URL::getHost, ConcurrentHashMap::new, u -> 1, Integer::sum));
+
+    List<Map.Entry<String, Integer>> entries = new ArrayList<>(map.entrySet());
+    entries.sort((e1, e2) -> e2.getValue() - e1.getValue());
+    entries.stream().filter(entry -> entry.getValue() >= 10).forEach(entry -> {
+      System.out.println(entry.getKey() + "," + entry.getValue());
+    });
+
+    System.out.println(entries.size() + " unique domains in " + links + " links from " + tweets + " tweets");
+    System.out.println(System.currentTimeMillis() - start);
+  }
+
+  public static <T, V> FlatMapper<T, V> squelch(ThrowFlatMapper<T, V> f) {
+    return (t, c) -> {
+      try {
+        f.flattenInto(t, c);
+      } catch (Exception e) {
+        squelchLog.log(Level.WARNING, String.valueOf(t), e);
+      }
+    };
+  }
+
+  interface ThrowFlatMapper<T, V> {
+    void flattenInto(T t, Consumer<V> c) throws Exception;
+  }
+}
---
+++ src/main/java/twitterprocessor/PullSpliterator.java
@@ -0,0 +1,137 @@
+package twitterprocessor;
+
+import java.util.List;
+import java.util.Spliterator;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ForkJoinPool;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+/**
+ * Split a stream by round robining the elements.
+ * User: sam
+ * Date: 2/21/13
+ * Time: 6:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PullSpliterator<T> implements Spliterator<T> {
+
+  // Should be much bigger than the number of elements a thread can process in 1 ms.
+  private static final int BUFFER_SIZE = 10000;
+
+  // Maximum number of processor threads
+  private final int maxqueues;
+
+  // We share the list of queues and need access to the done flag
+  private final PullSpliterator<T> parent;
+
+  // This spliterators queue of elements to process
+  private final ArrayBlockingQueue<T> queue;
+
+  // The list of queues that are being processed
+  private final List<ArrayBlockingQueue<T>> queues;
+
+  // This is set to true when there are no more elements to enqueue from the stream
+  private volatile boolean done;
+
+  /**
+   * Converts a embarrassingly sequential stream into a parallel stream with
+   * Streams.parallelStream(new PullSpliterator<>(stream))
+   *
+   * Uses the available processors as an estimate for the number of queues to generate.
+   * You should experiment with this for your workload though.
+   *
+   * @param stream
+   */
+  public PullSpliterator(Stream<T> stream) {
+    this(stream, Runtime.getRuntime().availableProcessors());
+  }
+
+  /**
+   * Converts a embarrassingly sequential stream into a parallel stream with
+   * Streams.parallelStream(new PullSpliterator<>(stream))
+   *
+   * @param stream The sequential stream to convert
+   * @param maxqueues The number of spliterators to generate for this workload
+   */
+  public PullSpliterator(Stream<T> stream, int maxqueues) {
+    this.maxqueues = maxqueues;
+    queues = new CopyOnWriteArrayList<>();
+    queue = new ArrayBlockingQueue<>(BUFFER_SIZE);
+    queues.add(queue);
+    parent = this;
+    ForkJoinPool.commonPool().execute(() -> {
+      int[] i = new int[1];
+      stream.forEach(element -> {
+        if (element != null) {
+          try {
+            // Uneven queues can cause put to block
+            // with little chance of recovery
+            ArrayBlockingQueue<T> queue;
+            do {
+              int size = queues.size();
+              queue = queues.get(i[0]++ % size);
+            } while (!queue.offer(element));
+          } catch (Throwable e) {
+            e.printStackTrace();
+          }
+        }
+      });
+      done = true;
+    });
+  }
+
+  // Creates a new split and adds itself to the parent
+  private PullSpliterator(List<ArrayBlockingQueue<T>> queues, int maxqueues, PullSpliterator<T> parent) {
+    this.queues = queues;
+    this.maxqueues = maxqueues;
+    this.parent = parent;
+    this.queue = new ArrayBlockingQueue<>(BUFFER_SIZE);
+    queues.add(queue);
+  }
+
+  /**
+   * Spin on the queue until something shows up in it.
+   *
+   * @param action Call accept on this when present
+   * @return false if nothing left, true if you processed something
+   */
+  @Override
+  public boolean tryAdvance(Consumer<? super T> action) {
+    int sleep = 1;
+    while (true) {
+      T poll = queue.poll();
+      if (poll != null) {
+        action.accept(poll);
+        return true;
+      }
+      if (parent.done) {
+        System.out.println("Optimized sleep value: " + sleep);
+        return false;
+      }
+      // Sadly this is way faster than poll(timeout) ~ 3%
+      // Automatically adjusts for the ratio between source speed
+      // and the speed at which we can process them
+      try {
+        Thread.sleep(sleep++);
+      } catch (InterruptedException e) {
+        // ignore
+      }
+    }
+  }
+
+  @Override
+  public int characteristics() {
+    return CONCURRENT;
+  }
+
+  @Override
+  public Spliterator<T> trySplit() {
+    if (queues.size() < maxqueues) {
+      return new PullSpliterator<>(queues, maxqueues, parent);
+    }
+    return null;
+  }
+
+}
